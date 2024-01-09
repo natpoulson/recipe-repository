a1a23c4f6a2550d72e689d3c7fc6f875acc1a25d
Nat Poulson <nat.poulson@outlook.com.au>
2024-01-10 05:57:04 +1100

    Annotate promise-based vs async...await functions

--- assets/js/proto.recipe.js
+++ assets/js/proto.recipe.js
@@ -1,24 +1,34 @@
+// Functions with promise-based searches are called normally
 function promiseSearch(query, resultCap = 10) {
-    const output = fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&addRecipeInformation=true&number=${resultCap}`, {
+    // The fetch function needs to be returned either directly or via a variable
+    // Otherwise the function will be returned as undefined
+    return fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&addRecipeInformation=true&number=${resultCap}`, {
+        // Setting values for the Request object, which will use default values for anything else
         method: "GET",
         headers: {
             "x-api-key": 'd6b7732ac8f6419095e86a0d96cc3570'
         }
     })
+    // Handle the response when successful
     .then(resp => {
+        // Check if the status code is anything other than 2XX using the Response.OK helper property
         if (!resp.ok) {
+            // You can perform error handling in here, or just pass the error back to the caller so it can be handled depending on use case (in this case)
             const errorMask = `[${resp.status}]: ${resp.statusText}`;
             throw new Error(errorMask);
         }
 
+        // Return a promise for the payload in JSON format
         return resp.json();
     });
-
-    return output;
 }
 
+// Functions using async...await syntax must be declared as async methods
 async function awaitSearch(query, resultCap = 10) {
+    // Because async...await abstracts a lot of the flow control away that you'd need to deal with using promises directly, you can intercept and apply error checking using try...catch syntax
+    // try...catch is more standard syntax for error checking, so this is easier to read
     try {
+        // Using await forces execution of asynchronous code to halt until a response is received, making it behave more like regular code
         const resp = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&addRecipeInformation=true&number=${resultCap}`, {
             method: "GET",
             headers: {
@@ -26,17 +36,19 @@ async function awaitSearch(query, resultCap = 10) {
             }
         });
 
+        // You can apply the checks on the response directly instead of encapsulating in .then()
         if (!resp.ok) {
             throw new Error (resp.statusText);
         }
 
+        // When done, simply return the promised JSON object
         return await resp.json();
 
     } catch (err)
     {
+        // We can handle the errors internally here
         console.error(err);
     }
-
 }
 
 // Example of handling the call through direct promise handling
