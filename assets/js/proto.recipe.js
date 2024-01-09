dd0be0abd743d92382925934d0aa48cc132ea671
Nat Poulson <nat.poulson@outlook.com.au>
2024-01-10 03:33:05 +1100

    Experiment with setting up connection to API and returning objects

--- assets/js/proto.recipe.js
+++ assets/js/proto.recipe.js
@@ -1,21 +1,22 @@
-function recipeSearch(query, resultCap = 10) {
-    let results;
-    try {
-        fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&addRecipeInformation&number=${resultCap}`, {
-            method: "GET",
-            headers: {
-                "x-api-key": 'd6b7732ac8f6419095e86a0d96cc3570'
-            }
-        })
-        .then((resp) => {
-            if (resp.ok) {
-                resp.json().then(data => {results = data});
-            }
-            throw resp.statusText;
-        });
-    } catch(err) {
-        console.error("Spoonacular API returned the following error:", err);
-    }
-    
-    console.log(results);
+async function recipeSearch(query, resultCap = 10) {
+    const output = fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&addRecipeInformation=true&number=${resultCap}`, {
+        method: "GET",
+        headers: {
+            "x-api-key": 'd6b7732ac8f6419095e86a0d96cc3570'
+        }
+    })
+    .then(resp => {
+        if (!resp.ok) {
+            const errorMask = `[${resp.status}]: ${resp.statusText}`;
+            // console.error(errorMask);
+            throw new Error(errorMask);
+        }
+        return resp.json();
+    });
+
+    return output;
 }
+
+recipeSearch("beef", 2)
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
