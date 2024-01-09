ef3eb078ed2baefd80f25e15c23e91a9c0938452
Nat Poulson <nat.poulson@outlook.com.au>
2024-01-10 04:43:02 +1100

    Experimented with implementing the function in both promise-based function and async/await

--- assets/js/proto.recipe.js
+++ assets/js/proto.recipe.js
@@ -1,4 +1,4 @@
-async function recipeSearch(query, resultCap = 10) {
+function promiseSearch(query, resultCap = 10) {
     const output = fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&addRecipeInformation=true&number=${resultCap}`, {
         method: "GET",
         headers: {
@@ -8,15 +8,49 @@ async function recipeSearch(query, resultCap = 10) {
     .then(resp => {
         if (!resp.ok) {
             const errorMask = `[${resp.status}]: ${resp.statusText}`;
-            // console.error(errorMask);
             throw new Error(errorMask);
         }
+
         return resp.json();
     });
 
     return output;
 }
 
-recipeSearch("beef", 2)
+async function awaitSearch(query, resultCap = 10) {
+    try {
+        const resp = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&addRecipeInformation=true&number=${resultCap}`, {
+            method: "GET",
+            headers: {
+                "x-api-key": 'd6b7732ac8f6419095e86a0d96cc3570'
+            }
+        });
+
+        if (!resp.ok) {
+            throw new Error (resp.statusText);
+        }
+
+        return await resp.json();
+
+    } catch (err)
+    {
+        console.error(err);
+    }
+
+}
+
+// Example of handling the call through direct promise handling
+/*
+promiseSearch("beef", 2)
     .then(data => console.log(data))
     .catch(error => console.error(error));
+*/
+
+// Example of handling the call from async...await
+/* 
+awaitSearch("pork", 1)
+    .then(data => console.log(data));
+*/
+
+// The key difference? You can apply error handling within the function itself using standard try...catch syntax without it being much of a hassle.
+// No matter which method you go with, successful executions still need to be handled with .then()
