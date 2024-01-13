01903f78648428b69b727c20e6cbff48545f824a
Nat Poulson <nat.poulson@outlook.com.au>
2024-01-13 18:27:51 +1100

    Base implementation of Recipe object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,134 @@
+class Recipe {
+  // Class properties
+  id = 0;
+  name = "";
+  image = "";
+  description = "";
+  servings = 0;
+  time = 0;
+  instructions = [];
+  ingredients = [];
+  hasMeat = false;
+  hasDairy = false;
+  hasGluten = false;
+  source = "";
+
+  // Constructor
+  constructor(id, properties = {}, importMode = false) {
+    this.id = id;
+    // Create a mapping for keys and values
+    // Ideally we would pass this in as an argument, but we're limiting the scope to spoonacular for now
+    const propMap = {
+      'name':'title',
+      'image':'image',
+      'description':'summary',
+      'servings':'servings',
+      'time':'readyInMinutes',
+      'hasMeat':'vegetarian',
+      'hasDairy':'dairyFree',
+      'hasGluten':'glutenFree',
+      'source':'source'
+    }
+    // Handling in cases where importMode is enabled
+    if (importMode) {
+      this.name = properties['name'];
+      this.image = properties['image'];
+      this.description = properties['description'];
+      this.servings = properties['servings'];
+      this.time = propertes['time'];
+      this.instructions = properties['instructions'];
+      this.ingredients = properies['ingredients'];
+      this.hasMeat = properties['hasMeat'];
+      this.hasDairy = properties['hasDairy'];
+      this.hasGluten = properties['hasGluten'];
+      this.source = properties['source'];
+      return;
+    }
+
+    // Populate the properties found, mapping each object by the names in the key value pair
+    for (const prop of Object.entries(propMap)) {
+      // Check that the property exists before we apply anything
+      if (Object.keys(properties).includes(prop[1]))
+      {
+        // Object.entries returns an array of the key and value E.g. [ 'name', 'title' ]
+        // Thus, we're using these to assign the equivalent values in the object.
+        this[prop[0]] = properties[prop[1]];
+      }
+    }
+
+    // Ensure the object being passed has instructions
+    if (Object.keys(properties).includes('analyzedInstructions')) {
+      // Extract the steps for processing
+      for (const instruction of properties['analyzedInstructions']['steps']) {
+        // Create a temporary object to synthesise a 'step', since we can't directly create an object with that property
+        const newInstruction = {};
+        // Inject a step in the format { '0':'Preheat oven to 200F' }
+        // This is so we can keep track of the step count using the key when sorting the array
+        newInstruction[instruction['number']] = instruction['step'];
+        // Push the sysnthesised object to the instructions array
+        this.instructions.push(newInstruction);
+      }
+      // Sort in ascending order by comparing the extracted values of the steps
+      this.instructions.sort((a, b) => {
+        return Object.values(a)[0] < Object.values(b)[0];
+      });
+    }
+    
+    // Invert the values of the booleans, since spoonacular's keys are inversions
+    this.hasMeat = !this.hasMeat;
+    this.hasDairy = !this.hasDairy;
+    this.hasGluten = !this.hasGluten;
+  }
+
+  // Getters
+  get template() {
+      return `<div class="row" style="padding-left: 50px; padding-right: 50px">
+      <div class="col s12 m6 l3">
+        <div class="card recipe-card">
+          <div class="card-image">
+            <img src="${this.image}" alt="${this.name}" />
+            <div class="recipe-card-badges">
+              <a class="btn-floating waves-effect waves-light red recipe-card-badge">
+                <i class="material-icons">favorite</i></a>
+              <a class="btn-floating waves-effect waves-light red accent-1">
+                <i class="material-icons">volume_up</i></a>
+            </div>
+          </div>
+          <div class="card-content recipe-card-content">
+            <span class="card-title">${this.name}</span>
+            <p>
+              ${this.description}
+            </p>
+            <div class="recipe-card-details">
+              <div>
+                <a
+                  class="btn-floating waves-effect waves-light red accent-1"
+                  ><i class="material-icons">schedule</i></a>
+                <p>${this.time}</p>
+              </div>
+              <div>
+                <a
+                  class="btn-floating waves-effect waves-light red accent-1"
+                  ><i class="material-icons">restaurant</i></a>
+                <p>${this.servings}</p>
+              </div>
+            </div>
+            <div style="padding-top: 15px; text-align: center">
+              <button
+                style="width: 100%"
+                class="waves-effect waves-light btn-large"
+                id="readMoreBtn"
+              >
+                Read More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  }
+}
+ 
 /* initiate materialize */
 $(document).ready(function () {
   $(".modal").modal();
